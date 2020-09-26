8be1ce3c54c5e25ba96d52d46be6bb9b3ad52200
Prinaya Choubey <pchoubey@u.rochester.edu>
2020-09-26 01:21:52 -0400

    Initial Commit

---
+++ FallColors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FallColors
+//
+//  Created by Prinaya Choubey on 9/26/20.
+//  Copyright © 2020 Prinaya Choubey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
