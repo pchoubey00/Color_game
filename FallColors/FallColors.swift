41d5f6bcae0193b14a2fedf90c046d3625b3a15a
Prinaya Choubey <pchoubey@u.rochester.edu>
2020-09-27 14:08:16 -0400

    First commit

---
+++ FallColors/FallColors.swift
@@ -0,0 +1,55 @@
+//
+//  FallColors.swift
+//  FallColors
+//
+//  Created by Prinaya Choubey on 9/26/20.
+//  Copyright © 2020 Prinaya Choubey. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FallColors{
+let ColorDict = [1 : UIColor.red,
+          2 : UIColor.green,
+          3 : UIColor.blue,
+          4 : UIColor.yellow,
+          5 : UIColor.black,
+          6 : UIColor.orange,
+  7 : UIColor.purple,
+  9 : UIColor.systemPink
+  ]
+  var score = 0
+  
+  
+ func randomColorGenerator() -> UIColor{
+     let randomNumber = Int.random(in: 1...9)
+      var outputColor = UIColor.clear
+      for (key, value) in ColorDict{
+          if key == randomNumber{
+              outputColor = value
+          }
+      }
+      
+      return outputColor
+  }
+    func randomfloat() -> CGFloat{
+        return CGFloat(Float(arc4random())/Float(UINT32_MAX))
+    }
+    
+    func randomColor() -> UIColor{
+        return UIColor(red: randomfloat(), green: randomfloat(), blue: randomfloat(), alpha: 1.0)
+    }
+    func increaseNumber(inputInt: Int) -> Int{
+       var Score = inputInt + 1
+        return Score
+    }
+    func decreaseNumber(inputInt: Int) -> Int{
+        var Score = inputInt + 1
+         return Score
+     }
+  
+
+}
+
+
--- FallColors/ViewController.swift
+++ FallColors/ViewController.swift
@@ -9,12 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    @IBOutlet weak var Box1: UIView!
+    
+    @IBOutlet weak var Box2: UIView!
+    
+    @IBOutlet weak var ScoreLabel: UILabel!
+   
+    @IBOutlet weak var Score_numberLabel: UILabel!
+    
+    @IBOutlet weak var step1: UILabel!
+    
+    @IBOutlet weak var step2: UILabel!
+    var score = 0
+   
+   
+    @IBOutlet weak var InstructionLabel: UILabel!
+    var model = FallColors()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        Box1.backgroundColor = UIColor.orange
+        Box2.backgroundColor = UIColor.orange
+        view.backgroundColor = UIColor.white
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap1))
+        
+
+        Box1.addGestureRecognizer(tapGesture)
+        let tapGesture2 = UITapGestureRecognizer(target: self, action: #selector(handleTap2))
+        Box2.addGestureRecognizer(tapGesture2)
+        ScoreLabel.text = NSLocalizedString("ScoreLabel_str", comment: "ScoreLabel")
+        step1.text = NSLocalizedString("Step1_str", comment: "Step 1" )
+        step2.text = NSLocalizedString("Step2_str", comment: "Step 2")
+        InstructionLabel.text = NSLocalizedString("Instruction_name", comment: "Instruction Name")
+        
+        Score_numberLabel.text = String(score)
+        
+        
+       
     }
-
-
+    @objc func handleTap1(){
+        Box1.backgroundColor = model.randomColorGenerator()
+        if Box1.backgroundColor == Box2.backgroundColor{
+        score = score + 1
+        }
+        Score_numberLabel.text = String(score)
+    }
+    @objc func handleTap2(){
+        Box2.backgroundColor = model.randomColorGenerator()
+        if Box1.backgroundColor == Box2.backgroundColor{
+             score = score + 1
+             }
+       
+        Score_numberLabel.text = String(score)
+    }
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.backgroundColor = model.randomColor()
+        score = score - 1
+        Score_numberLabel.text = String(score)
+        
+    
+    }
+ 
+    
 }
 
